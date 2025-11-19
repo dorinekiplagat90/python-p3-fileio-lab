62805862417ba199215350b9df062141ce24c6be
dorinekiplagat90 <dorinekiplagat90@gmail.com>
2025-11-20 00:13:40 +0300

    addded a file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    # Always add .txt extension
+    path = f"{file_name}.txt"
+    with open(path, "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    # Always add .txt extension
+    path = f"{file_name}.txt"
+    with open(path, "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    # Always add .txt extension
+    path = f"{file_name}.txt"
+    with open(path, "r") as f:
+        return f.read()
